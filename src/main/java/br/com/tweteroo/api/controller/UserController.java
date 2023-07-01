a45b385c32248a12c1ab58477520737bfd299bf2
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-07-01 20:18:13 -0300

    feat: user controller - post sign-up request

---
+++ src/main/java/br/com/tweteroo/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package br.com.tweteroo.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tweteroo.api.dto.UserDTO;
+import br.com.tweteroo.api.service.UserService;
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/sign-up")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+
+	@PostMapping
+	public ResponseEntity<Void> signUp(@RequestBody @Valid UserDTO req) {
+		service.signUp(req);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+}
