3c0462a52d52c7413f30ba86cb7a8187ab090896
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-07-01 18:51:58 -0300

    first commit

---
+++ src/main/java/br/com/tweteroo/api/TweterooApplication.java
@@ -0,0 +1,13 @@
+package br.com.tweteroo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweterooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweterooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tweteroo/api/TweterooApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tweteroo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweterooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
