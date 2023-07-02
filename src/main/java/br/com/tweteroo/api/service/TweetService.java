fe7fb948f9ca29d64a4518948b43f9bdef6c097a
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-07-02 18:49:56 -0300

    feat: find all tweets with pagination service

--- src/main/java/br/com/tweteroo/api/service/TweetService.java
+++ src/main/java/br/com/tweteroo/api/service/TweetService.java
@@ -1,6 +1,9 @@
 package br.com.tweteroo.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import br.com.tweteroo.api.dto.TweetDTO;
@@ -13,6 +16,12 @@ public class TweetService {
 
 	@Autowired
     private TweetRepository repository;
+	
+	public Page<Tweet> findAll(int page) {
+		int pageSize = 5;
+        PageRequest pageRequest = PageRequest.of(page, pageSize, Sort.by(Sort.Direction.DESC, "id"));
+        return repository.findAll(pageRequest);
+	}
 	
 	public void createTweet(@Valid TweetDTO req) {
 		repository.save(new Tweet(req));
