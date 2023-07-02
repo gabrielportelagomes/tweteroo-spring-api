5e83fc975e291d9af25a6bef896e15d98efdc540
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-07-02 19:04:26 -0300

    feat: find tweets by username

--- src/main/java/br/com/tweteroo/api/repository/TweetRepository.java
+++ src/main/java/br/com/tweteroo/api/repository/TweetRepository.java
@@ -1,9 +1,12 @@
 package br.com.tweteroo.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.tweteroo.api.model.Tweet;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 
+	List<Tweet> findByUsername(String username);
 }
