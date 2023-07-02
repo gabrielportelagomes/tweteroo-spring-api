10acb8e62bf3c768e61ef49f76b4d6711a7808ed
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-07-02 18:11:23 -0300

    refactor: service annotation added

--- src/main/java/br/com/tweteroo/api/service/TweetService.java
+++ src/main/java/br/com/tweteroo/api/service/TweetService.java
@@ -1,12 +1,14 @@
 package br.com.tweteroo.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.tweteroo.api.dto.TweetDTO;
 import br.com.tweteroo.api.model.Tweet;
 import br.com.tweteroo.api.repository.TweetRepository;
 import jakarta.validation.Valid;
 
+@Service
 public class TweetService {
 
 	@Autowired
