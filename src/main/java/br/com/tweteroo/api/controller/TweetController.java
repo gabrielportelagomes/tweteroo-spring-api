d3b4c4046292ea6ee0c759ec80e04ba8be40aeb8
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-07-02 18:50:09 -0300

    feat: find all tweets with pagination controller

--- src/main/java/br/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/br/com/tweteroo/api/controller/TweetController.java
@@ -1,15 +1,19 @@
 package br.com.tweteroo.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tweteroo.api.dto.TweetDTO;
+import br.com.tweteroo.api.model.Tweet;
 import br.com.tweteroo.api.service.TweetService;
 import jakarta.validation.Valid;
 
@@ -18,9 +22,13 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/tweets")
 public class TweetController {
 
-	
 	@Autowired
 	private TweetService service;
+
+	@GetMapping
+	public Page<Tweet> getTweets(@RequestParam("page") int page) {
+		return service.findAll(page);
+	}
 
 	@PostMapping
 	public ResponseEntity<Void> createTweet(@RequestBody @Valid TweetDTO req) {
